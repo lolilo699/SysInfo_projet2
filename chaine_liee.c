84797e4bfbea0be930a7a6e1e4ba81d5b5bb7324
jdoutreloux <justine.doutreloux@hotmail.be>
2015-04-29 23:12:51 +0200

    Modification de chaine_liee.c

--- chaine_liee.c
+++ chaine_liee.c
@@ -7,3 +7,40 @@
 //
 
 #include <stdio.h>
+#include <inttypes.h>
+
+void add_node(Node *liste, Node *n)
+{
+    if(liste == NULL)
+    {
+        return;
+    }
+    while (liste->next != NULL)
+    {
+        liste = liste->next;
+    }
+    liste->next = n;
+}
+
+void add_number(Node *liste, uint32_t n)
+{
+    if(liste == NULL)
+    {
+        return;
+    }
+    while (liste->next != NULL)
+    {
+        if (liste->nombre == n)
+        {
+            liste->count++;
+            a=1;
+            return;
+        }
+        else
+        {
+            liste=liste->next;
+        }
+    }
+    Node new = {n, 1, NULL, 'file'};
+    add_node(liste, &new);
+}
