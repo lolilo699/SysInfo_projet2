a6bd3ae9778f2ebe03c0362de86b4ec62a4d2ee1
jdoutreloux <justine.doutreloux@hotmail.be>
2015-05-01 15:12:54 +0200

    Ajout des signatures des trois fonctions de bases de producteur.c

--- producteur.c
+++ producteur.c
@@ -1,6 +1,25 @@
 //
-//  producteur.c
+//  producteur.c doit gérer l'ouverture, la lecture et la fermeture d'un fichier, et l'écriture des nombres lus dans le fichier dans une liste. Il doit aussi gérer les cas où l'entrée n'est pas un fichier mais une URL ou un descripteur de fichier.
 //  
 
 
 #include <stdio.h>
+
+
+
+
+
+uint32_t[] producteur_fichier(char fichier[])
+{
+    
+}
+
+uint32_t[] producteur_URL(char url[])
+{
+    
+}
+
+uint32_t[] producteur_descripteur(int fd)
+{
+    
+}
