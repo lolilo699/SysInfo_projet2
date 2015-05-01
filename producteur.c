f0a6404893889e5c5018520318dbcd37291e528f
jdoutreloux <justine.doutreloux@hotmail.be>
2015-05-01 15:54:01 +0200

    Ajout du code de producteur_descripteur

--- producteur.c
+++ producteur.c
@@ -8,14 +8,46 @@
 
 struct Node()
 {
-    uint32_t nombre;
+    uint64_t nombre;
     Node *next;
 }
 
 
 Node* producteur_descripteur(int fd)
 {
-    
+    uint64_t nombre;
+    Node *premier;
+    premier->nombre = 0;
+    premier->next = NULL;
+    Node *courant;
+    courant=premier;
+    while(true)
+    {
+        int bytes;
+        bytes = read(fd, &nombre, sizeof(uint64_t));
+        if(bytes==8)
+        {
+            nombre = be64toh(nombre);
+            if(premier==courant)
+            {
+                premier->nombre = nombre;
+            }
+            else
+            {
+                Node *n;
+                n->nombre = nombre;
+                n->next = NULL;
+                courant->next = n;
+                courant = courant->next;
+            }
+            
+        }
+        else
+        {
+            printf("Erreur");
+        }
+    }
+    return premier;
 }
 
 
