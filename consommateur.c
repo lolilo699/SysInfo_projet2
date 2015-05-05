4fddd4632d8da3841f754df6d25a085697a6723b
jdoutreloux <justine.doutreloux@hotmail.be>
2015-05-05 14:19:54 +0200

    Ajout du code de factorisation en nombres premiers

--- consommateur.c
+++ consommateur.c
@@ -2,4 +2,37 @@
 //  consommateur.c
 //  
 
+#include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
+#include <errno.h>
+#include <stdint.h>
+
+void prime(unsigned int n, unsigned int d)
+{
+    if (n != 1)
+    {
+        if (n % d)
+        {
+            d++;
+            f(n, d);
+        }
+        else
+        {
+            liste_facteurs[i] = d;
+            i++;
+            f(n / d, d);
+        }
+    }
+}
+int conso(void)
+{
+    unsigned int n;
+    int k;
+    scanf("%u", &n);
+    prime(n, 2);
+    for (k =0; k < i; k++)
+        printf("%d ", liste_facteurs[k]);
+    printf("\n");
+    return 0;
+}
